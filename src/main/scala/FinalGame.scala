7e0a3c4f84b877d2bf99cc6f24dd424bbf6687f4
Ilze <puisaane,ilze@gmail.com>
2021-10-14 21:02:30 +0300

    3rd modification

--- src/main/scala/FinalGame.scala
+++ src/main/scala/FinalGame.scala
@@ -1,90 +1,97 @@
+import scala.io.StdIn.readLine
 
 package object tictactoe {
-  val playerX = 'X' //Ilze changed the name
-  val playerO = 'O' //Ilze changed the name
-  val BaseBoard = ('1' to '9').toList
-  val WinnerLines = List((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-  //val randomGen = new util.Random(System.currentTimeMillis) - deleted
+  val playerX = 'X'
+  val playerO = 'O'
+  val playerXName = readLine(s"You will be player X. What is your name?") //Ilze added a line
+  val playerOName = readLine(s"You will be player O. What is your name?") //Ilze added a line
+  val winningOptions = List((0,3,6), (1,4,7), (2,5,8), (0,1,2), (3,4,5), (6,7,8), (0,4,8), (2,4,6))
+  val boardNumbers = ('1' to '9').toList
+
+  //deleted - val randomGen = new util.Random(System.currentTimeMillis)
 }
 
 package tictactoe {
 
   import scala.io.StdIn.readChar
 
-  class Board(aBoard : List[Char] = BaseBoard) {
+  class GameBoard(board:List[Char] = boardNumbers) {
 
-    def availableMoves = aBoard.filter(c => c != playerX && c != playerO)
 
-    def availableMovesIdxs = for ((c,i) <- aBoard.zipWithIndex if c != playerX && c != playerO) yield i
+    def movesLeft = board.filter(n => n != playerX && n != playerO)
 
+    //deleted - def movesLeftIndex = for ((n,i) <- board.zipWithIndex if n != playerX && n != playerO) yield i
     //deleted - def computerPlays = new Board(aBoard.updated(availableMovesIdxs(randomGen.nextInt(availableMovesIdxs.length)), playerO))
-    def playerOplays(move : Char) = new Board(aBoard.updated(aBoard.indexOf(move), playerO)) //Ilze changed the name
-    // and replaced the computer move with 2nd player move
 
-    def playerXplays(move : Char) = new Board(aBoard.updated(aBoard.indexOf(move), playerX)) //Ilze changed the name
+    def playerXplays(move: Char) = new GameBoard(board.updated(board.indexOf(move), playerX))
 
-    def isDraw = aBoard.forall(c => c == playerX || c == playerO)
+    def playerOplays(move: Char) = new GameBoard(board.updated(board.indexOf(move), playerO)) // replaced the computer move with 2nd player move
 
-    def isWinner(winner : Char) =
-      WinnerLines.exists{case (i,j,k) => aBoard(i) == winner && aBoard(j) == winner && aBoard(k) == winner}
 
-    def isOver = isWinner(playerO) || isWinner(playerX) || isDraw
 
-    def print {
-      aBoard.grouped(3).foreach(row => println(row(0) + " " + row(1) + " " + row(2)))
+    def winner(winner: Char) =
+      winningOptions.exists{case (i,j,k) => board(i) == winner && board(j) == winner && board(k) == winner}
+
+    def draw = board.forall(c => c == playerX || c == playerO)
+
+    def gameOver = winner(playerO) || winner(playerX) || draw
+
+    def boardPrint {
+      board.grouped(3).foreach(row => println(row(0) + " - " + row(1) + " - " + row(2))) //Ilze changed the appearance of the board
     }
 
-    def printOverMessage {
-      if (isWinner(playerX)) println("PLAYER X WINS.") //Ilze changed the println
-      else if (isWinner(playerO)) println("PLAYER 0 WINS.") //Ilze changed the println
-      else if (isDraw) println("A draw - there is no winner.") //Ilze changed the println
-      //else println("Not over yet, or something went wrong.") - deleted
+    def gameOverPrint {
+      if (winner(playerX)) println(s"Congrats $playerXName, you won!") //Ilze changed the println
+      else if (winner(playerO)) println(s"Congrats $playerOName you won!") //Ilze changed the println
+      else if (draw) println("A draw - there is no winner.") //Ilze changed the println
+      //deleted - else println("Something went wrong.")
     }
 
   }
 
+  object FinalGame extends App { //need to rename
 
-  object FinalGame extends App {
+    println("     LET'S PLAY TIC-TAC-TOE!") //Ilze changed the println
+    println
 
-    println("LET'S PLAY TIC-TAC-TOE! HERE IS THE STARTING BOARD WITH NUMBERS") //Ilze changed the println
-
-    def play(board : Board, turn : Char) {
+    def play(board: GameBoard, turn: Char) {
 
       //Reads a char from input until it is one of
       //the available moves in the current board
-      def clampMove() : Char = {
+      def clampMove(): Char = {
 
-        print("Choose your move: ") ////Ilze changed the println
-        val validMoves = board.availableMoves
+        print("Choose a number from the board above -> ") ////Ilze changed the println
+        val validNumbers = board.movesLeft
         val move = readChar
-        if (validMoves.contains(move)) {
+        if (validNumbers.contains(move)) {
           move
         } else {
-          println("Invalid move. Choose another one from " + validMoves)
+          println("Wrong number! Choose another one from this" + validNumbers)
           clampMove()
         }
       }
 
+      println
+      board.boardPrint
 
-      board.print
 
-      if (board.isOver) {
-        board.printOverMessage
+      if (board.gameOver) {
+        board.gameOverPrint
         return
       }
 
       if (turn == playerX) {
-        println("** PLAYER X TURN **") //Ilze changed the println
+        println("   ***  PLAYER X TURN  ***   ") //Ilze changed the println
         val nextBoard = board.playerXplays(clampMove)
         play(nextBoard, playerO)
       } else {
-        println("** PLAYER O TURN **") //Ilze changed the println
+        println("   ***  PLAYER O TURN  ***   ") //Ilze changed the println
         val nextBoard = board.playerOplays(clampMove)
         play(nextBoard, playerX)
       }
     }
 
-    play(new Board(),playerX)
+    play(new GameBoard(),playerX)
 
   }
 
